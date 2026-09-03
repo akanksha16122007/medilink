9a548f959c781a62e82f2b481d92634bef82f76f
akanksha16122007 <akankshakhurana01@gmail.com>
2026-09-04 02:05:19 +0530

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import Header from "./Components/Header";
 function App() {
   return (
-    <div className="App">
-      <h1>MediLink</h1>
+    <div>
+      <Header />
     </div>
   );
 }
