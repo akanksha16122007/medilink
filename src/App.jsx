6d1252bea1e010a496de9ddabcfbc2d7389dd2d4
akanksha16122007 <akankshakhurana01@gmail.com>
2026-09-04 14:50:20 +0530

    ADDED LOGIN AND PROFILE ON WEB-PAGE

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Login from "./Pagess/Login";
+import Profile from "./Pagess/Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
 
-      <Footer />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
