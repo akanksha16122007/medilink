ebd20162d4dbd040aac5868813ae3b6ab1507a07
jahnvi <jahnvirattan@gmail.com>
2026-09-04 20:09:51 +0530

    Update header navigation and responsive records styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <div>
       <h1>MediLink</h1>
-      <button>Home</button>
+
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+
       <button>Dashboard</button>
-      <button>Records</button>
+
+      <Link to="/records">
+        <button>Records</button>
+      </Link>
+
       <Link to="/profile">
         <button className="profile">Profile</button>
       </Link>
+
       <Link to="/login">
         <button className="login">Login</button>
       </Link>
     </div>
   );
 };
+
 export default Header;
