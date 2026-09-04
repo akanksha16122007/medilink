b4d1c0a1f93006573257bf5ad4b73e3cd850f449
jahnvi <jahnvirattan@gmail.com>
2026-09-04 17:00:45 +0530

    Resolve App.jsx merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+```jsx
 import "./App.css";
+
 import Header from "./Components/Header";
+
 import Footer from "./Components/Footer";
+
 import Login from "./Pagess/Login";
+
 import Profile from "./Pagess/Profile";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,11 +16,11 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header />
+
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
-
 
         <Footer />
       </BrowserRouter>
@@ -23,3 +29,4 @@ function App() {
 }
 
 export default App;
+```
