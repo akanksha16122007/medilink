8dd2bba4ab5ae2b0d5724faa578b579e67a58fa8
akanksha16122007 <akankshakhurana01@gmail.com>
2026-09-05 19:20:07 +0530

    added log out button

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const user = JSON.parse(localStorage.getItem("user"));
 
+  // const [LoggedIn, setLoggedIn] = useState(
+  //   localStorage.getItem("isLoggedIn") === "true",
+  // );
+  const navigate = useNavigate();
+  const [showLogOut, setshowLogOut] = useState(false);
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+
+    setshowLogOut(false);
+
+    navigate("/login");
+  };
+
   return (
     <header className="header">
-
       {/* Logo */}
       <Link to="/" className="logo">
         <span className="logo-icon">✚</span>
@@ -16,16 +28,12 @@ const Header = () => {
       </Link>
 
       {/* Hamburger button */}
-      <button
-        className="menu-btn"
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
+      <button className="menu-btn" onClick={() => setMenuOpen(!menuOpen)}>
         ☰
       </button>
 
       {/* Navigation */}
       <nav className={menuOpen ? "nav active" : "nav"}>
-
         <Link to="/" onClick={() => setMenuOpen(false)}>
           Home
         </Link>
@@ -45,16 +53,28 @@ const Header = () => {
         {/* Login / User */}
         {user ? (
           <div className="user-info">
-            <img
-              src={
-                user?.profilePic ||
-                "https://i.pinimg.com/736x/db/a9/ce/dba9ce433113b68e719456b6d78c4f2a.jpg"
-              }
-              alt="Profile"
-              className="profile-pic"
-            />
+            <div
+              className="profile-section"
+              onClick={() => setshowLogOut(!showLogOut)}
+            >
+              <img
+                src={
+                  user?.profilePic ||
+                  "https://i.pinimg.com/736x/db/a9/ce/dba9ce433113b68e719456b6d78c4f2a.jpg"
+                }
+                alt="Profile"
+                className="profile-pic"
+              />
 
-            <span>{user.name}</span>
+              <span>{user.name}</span>
+            </div>
+            {showLogOut && (
+              <div className="logout-menu">
+                <button onClick={handleLogout}>
+                  <i className="fa-solid fa-arrow-right-from-bracket"></i>Logout
+                </button>
+              </div>
+            )}
           </div>
         ) : (
           <Link
@@ -65,9 +85,7 @@ const Header = () => {
             Login
           </Link>
         )}
-
       </nav>
-
     </header>
   );
 };
